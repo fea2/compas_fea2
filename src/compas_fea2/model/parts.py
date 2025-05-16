f55e170d345786d61e715f095c945aabf6f5f086
ines <champagne@vaulted-floors.ch>
2025-05-16 15:48:38 +0200

    part method (discretized line)

--- src/compas_fea2/model/parts.py
+++ src/compas_fea2/model/parts.py
@@ -18,6 +18,7 @@ from compas.geometry import Box
 from compas.geometry import Frame
 from compas.geometry import Plane
 from compas.geometry import Point
+from compas.geometry import Line
 from compas.geometry import Scale
 from compas.geometry import Transformation
 from compas.geometry import Vector
@@ -612,6 +613,63 @@ class _Part(FEAData):
     #                       Constructor methods
     # =========================================================================
 
+    @classmethod
+    def from_compas_lines_discretized(cls, lines, targetlength, element_class, section, frame, name=None, **kwargs):
+        """Generate a discretized model from a list of :class:`compas.geometry.Line`.
+
+        Parameters
+        ----------
+        compas_lines : dict
+            Dictionary providing for each compas line the targetlenght of the mesh, the element class, the section and frame. For example:
+            {'L1': [targetlenght1, BeamElement, section1, frame1], 'L2': [targetlenght2, BeamElement, section2, frame2]...}
+        target_length : int
+            The targetted lenght of the discretization of the lines.
+        section : :class:`compas_fea2.model.BeamSection`
+            The section to be assigned to the elements, by default None.
+        element_model : str, optional
+            Implementation model for the element, by default 'BeamElement'.
+        xaxis : list[float], optional
+            The x-axis direction, by default [0,1,0].
+        name : str, optional
+            The name of the part, by default None (one is automatically generated).
+
+        Returns
+        -------
+        :class:`compas_fea2.model.Part`
+            The part.
+
+        """
+        prt = cls(name=name)
+
+        for line in lines:
+            # initialization of first point of first element
+            p1 = Point(line.start.x, line.start.y, line.start.z)
+            prt.add_node(Node(xyz=[p1.x, p1.y, p1.z]))
+            
+            # initialization of second point of first element
+            n = int(line.length // targetlength) if line.length>targetlength else 1 
+            v_discretized = Vector(line.vector[0] / n, line.vector[1] / n, line.vector[2] / n)
+            p2 = p1.translated(v_discretized)
+
+            # loop to create the elements except the last one
+            for i in range(n-1):
+                prt.add_node(Node(xyz=[p2.x, p2.y, p2.z]))   
+                nodes = (list(prt.find_closest_nodes_to_point(p1, number_of_nodes=1)) or [Node(xyz=[p1.x, p1.y, p1.z])]) + (list(prt.find_closest_nodes_to_point(p2, number_of_nodes=1) or [Node(xyz=[p2.x, p2.y, p2.z])]))
+                element = element_class(nodes=nodes, section=section, frame=frame)
+                prt.add_element(element)
+
+                p1.translate(v_discretized)
+                p2.translate(v_discretized)
+                
+            #the last element might not respect the targetlength and is therefore created indepedantly
+            prt.add_node(Node(xyz=[line.end.x, line.end.y, line.end.z]))
+            nodes = (list(prt.find_closest_nodes_to_point(p1, number_of_nodes=1)) or [Node(xyz=[p1.x, p1.y, p1.z])]) + list(prt.find_closest_nodes_to_point(line.end, number_of_nodes=1) )
+            element = element_class(nodes=nodes, section=section, frame=frame) 
+            prt.add_element(element)
+
+        return prt
+
+    
     @classmethod
     def from_compas_lines(
         cls,
